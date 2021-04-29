5986fca56d8f0146875c239564e83eb80a448199
Signory Somsavath <ssomsavath@gmail.com>
2021-04-28 23:28:44 -0500

    Put all the exceptions in one methods

---
+++ mobile-app-ws/src/main/java/com/sigsomsavath/ws/exceptions/UserServiceExceptions.java
@@ -0,0 +1,13 @@
+package com.sigsomsavath.ws.exceptions;
+
+public class UserServiceExceptions extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8227593032551244649L;
+
+	public UserServiceExceptions(String message) {
+		super(message);
+	}
+
+}
