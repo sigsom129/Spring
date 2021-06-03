76d1f8d40a6df4647844af7251eae062a1711a15
Signory Somsavath <ssomsavath@gmail.com>
2021-06-03 02:01:49 -0500

    Validate RequestBody in createUser

--- PhotoAppApiUsers/src/main/java/com/sigsomsavath/photoapp/api/user/ui/controller/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/sigsomsavath/photoapp/api/user/ui/controller/UsersController.java
@@ -2,9 +2,14 @@ package com.sigsomsavath.photoapp.api.user.ui.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sigsomsavath.photoapp.api.users.ui.model.CreateUserRequestModel;
 
 @RestController
 @RequestMapping("/users")
@@ -16,6 +21,12 @@ public class UsersController {
 	@GetMapping("/status/check")
 	public String status() {
 		return "Working on port" + env.getProperty(" local.server.port");
+	}
+	
+	
+	@PostMapping
+	public String createUser(@Validated @RequestBody CreateUserRequestModel userDetails) {
+		return "Create user method is called";
 	}
 
 }
